9d7a019c09cef42c297bea75eefcaf0ec08b84c8
Sarmat Khachirov <skhachirov12@gmail.com>
2020-12-03 10:02:28 +0300

    feat: implement simple login controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
-import catchAsync from '../utils/catchAsync'
 import User from '../models/user'
+import catchAsync from '../utils/catchAsync'
+import AppError from '../utils/appError'
 
 export const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const user = await User.create({
@@ -9,6 +10,25 @@ export const signup = catchAsync(async (req: Request, res: Response, next: NextF
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
   })
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user
+    }
+  })
+
+  next()
+})
+
+export const login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400))
+  }
+
+  const user = await User.findOne({ email }).select('+password')
 
   res.status(200).json({
     status: 'success',
