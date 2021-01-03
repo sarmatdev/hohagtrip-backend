bdc2707f6f01027c5d3e0c91f60cb23a5e0f41ca
Sarmat Khachirov <skhachirov12@gmail.com>
2021-01-03 16:53:57 +0300

    feat: migrate to typegoose

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,101 +1,92 @@
 import crypto from 'crypto'
-import { model, Schema, Model, Document } from 'mongoose'
 import { NextFunction } from 'express'
 import validator from 'validator'
 import bcrypt from 'bcryptjs'
+import { prop, pre, getModelForClass } from '@typegoose/typegoose'
+import { instanceMethod } from 'typegoose'
 
-interface IUser extends Document {
-  name: string
-  email: string
-  password: string
-  passwordConfirm: string
+enum Role {
+  USER = 'user',
+  ADMIN = 'admin'
 }
 
-interface UserDocument extends IUser {
-  passwordChangedAt?: number
-  passwordResetToken?: string
-  passwordResetExpires?: number
-  active?: boolean
-}
-
-const UserSchema: Schema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Please tell us your name!']
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide your email'],
-    validate: [validator.isEmail, 'Please provide a valid email'],
-    unique: true,
-    lowercase: true
-  },
-  role: {
-    type: String,
-    enum: ['user', 'host', 'admin'],
-    default: 'user'
-  },
-  password: {
-    type: String,
-    required: [true, 'Please provide a password'],
-    minlength: 8,
-    select: false
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'Please confirm your password'],
-    validate: function (el: string) {
-      return el === this.password
-    }
-  },
-  passwordChangedAt: Date,
-  passwordResetToken: String,
-  passwordResetExpires: Date,
-  active: {
-    type: Boolean,
-    default: true,
-    select: false
-  }
-})
-
-UserSchema.pre<UserDocument>('save', async function (next: NextFunction) {
+@pre<User>('save', async function (next: NextFunction): Promise<void> {
   if (!this.isModified('password')) return next()
   this.password = await bcrypt.hash(this.password, 12)
   this.passwordConfirm = undefined
   next()
 })
-
-UserSchema.pre<UserDocument>('save', function (next: NextFunction): void {
+@pre<User>('save', function (next: NextFunction): void {
   if (!this.isModified('password') || this.isNew) return next()
   this.passwordChangedAt = Date.now() - 1000
   next()
 })
+class User {
+  @prop({ required: true })
+  public firstName!: string
 
-UserSchema.methods.correctPassword = async function (candidatePassword: string, userPassword: string) {
-  return await bcrypt.compare(candidatePassword, userPassword)
-}
+  @prop({ required: true })
+  public lastName!: string
 
-UserSchema.methods.changedPasswordAfter = function (ctx: UserDocument, JWTTimestamp) {
-  if (ctx.passwordChangedAt) {
-    //eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const changedTimestamp = parseInt(ctx.passwordChangedAt.getTime() / 1000, 10)
+  @prop({
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide your email'
+    }
+  })
+  public email!: string
 
-    return JWTTimestamp < changedTimestamp
+  @prop({ enum: Role, default: 'user' })
+  public role?: string
+
+  @prop({ required: [true, 'Please provide a password'], minlength: 8, select: false })
+  public password!: string
+
+  @prop({
+    required: [true, 'Please confirm your password'],
+    validate: function (el: string) {
+      return el === this.password
+    }
+  })
+  public passwordConfirm!: string
+
+  @prop()
+  public passwordChangedAt?: number
+
+  @prop({ type: String })
+  public passwordResetToken?: string
+
+  @prop()
+  public passwordResetExpires?: number
+
+  @prop({ type: Boolean, default: true, select: false })
+  public active?: boolean
+
+  @instanceMethod async correctPassword(candidatePassword: string, userPassword: string): Promise<boolean> {
+    return await bcrypt.compare(candidatePassword, userPassword)
   }
 
-  return false
+  @instanceMethod changedPasswordAfter(JWTTimestamp: number) {
+    if (this.passwordChangedAt) {
+      //eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const changedTimestamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10)
+
+      return JWTTimestamp < changedTimestamp
+    }
+
+    return false
+  }
+
+  @instanceMethod createPasswordResetToken(): string {
+    const resetToken = crypto.randomBytes(32).toString('hex')
+    this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000
+    return resetToken
+  }
 }
 
-UserSchema.methods.createPasswordResetToken = function (ctx: UserDocument): string {
-  const resetToken = crypto.randomBytes(32).toString('hex')
-
-  ctx.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex')
-  ctx.passwordResetExpires = Date.now() + 10 * 60 * 1000
-
-  return resetToken
-}
-
-const User: Model<UserDocument> = model<UserDocument>('User', UserSchema)
-
-export default User
+const UserModel = getModelForClass(User)
+export default UserModel
